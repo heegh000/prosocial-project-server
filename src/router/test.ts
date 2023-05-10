baf5da63c7fac9a16e721eecb66d43810d8e2273
Ubuntu <ubuntu@ip-172-31-40-176.ap-northeast-2.compute.internal>
2023-05-10 15:54:19 +0000

    first commit

---
+++ src/router/test.ts
@@ -0,0 +1,14 @@
+import { Router, Request, Response, response } from 'express';
+const router : Router = Router();
+
+router.get('/', async(req : Request, res : Response) => {
+    res.send("AAA")
+});
+
+router.post('/', async(req : Request, res : Response) => {
+    console.log("ASDSAD");
+    console.log(req.body);
+    res.send(req.body);
+});
+
+export { router };
